be524d4d3e2b1176fa56753e783da5218824de38
aramHernandez-douglasC <hernandezgardunoa@student.douglascollege.ca>
2020-11-22 15:56:05 -0800

    added update quantity item

--- Server/src/main/java/com/csis3275/project/SASRestaurantReservation/controller/OrderController.java
+++ Server/src/main/java/com/csis3275/project/SASRestaurantReservation/controller/OrderController.java
@@ -1,11 +1,26 @@
 package com.csis3275.project.SASRestaurantReservation.controller;
 
 import java.util.List;
+import static java.lang.System.out;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.csis3275.project.SASRestaurantReservation.model.MenuItem;
+import com.csis3275.project.SASRestaurantReservation.repository.MenuItemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,10 +66,26 @@ public class OrderController {
 	public void deleteSingleOrder(@RequestParam int id) {
 		this.orderItem = this.oItRepo.findById(id).get();
 		oItRepo.delete(this.orderItem);
-		System.out.println("Item deleted to:" + this.orderItem.getId());
+		out.println("Item deleted to:" + this.orderItem.getId());
 
 	}
-	
+
+	@PutMapping("/update-orderItem-quantity")
+	public void updateQuantity(@RequestParam int id, @RequestParam int qty) throws Throwable {
+
+		this.orderItem = this.oItRepo.findById(id).get();
+		try {
+			if (qty > 0) {
+				this.orderItem.setQuantity(qty);
+				this.oItRepo.save(this.orderItem);
+			} else {
+				out.println("Quantity cannot be less or equal 0");
+			}
+		}catch(Exception e) {
+			out.println(new Exception(e));
+		}
+
+	}
 
 	@GetMapping("/get-all/orders")
 	public List<Seat> findAllOrders() {
