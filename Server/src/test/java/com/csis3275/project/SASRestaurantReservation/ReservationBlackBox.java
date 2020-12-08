b4f06842c4a067be68c58dff39084e19209f001b
Seangg9 <seangg1999@gmail.com>
2020-12-07 20:45:13 -0800

    Created automated blackbox tests

---
+++ Server/src/test/java/com/csis3275/project/SASRestaurantReservation/ReservationBlackBox.java
@@ -0,0 +1,51 @@
+package com.csis3275.project.SASRestaurantReservation;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class ReservationBlackBox {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @Before
+  public void setUp() {
+	  System.setProperty("webdriver.chrome.driver", "C:/Users/Sean/Documents/chromeDriver/chromedriver.exe");
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void loginTest() {
+    driver.get("http://localhost:4200/");
+    driver.manage().window().setSize(new Dimension(974, 1040));
+    driver.findElement(By.linkText("Login")).click();
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).sendKeys("TestUser");
+    driver.findElement(By.id("password")).sendKeys("Test");
+    driver.findElement(By.cssSelector(".btn:nth-child(3)")).click();
+  }
+}
